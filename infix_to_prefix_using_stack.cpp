ca10fcc6a70289092209fb8b66b120c94156d04b
Tayyub Naveed <saneel1720403@gmail.com>
2021-02-20 15:17:58 +0500

    Add files via upload

---
+++ infix_to_prefix_using_stack.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool is_operator(char c){
+    if(c == '+' || c == '*' || c == '-' || c == '/')
+        return true;
+    else
+        return false;
+}
+
+int precedence(char c){
+    if(c == '^')
+        return 3;
+    else if(c == '*' || c == '/')
+        return 2;
+    else if(c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
+string infix_to_prefix(stack<char> s,string infix){
+    string prefix;
+    //reversing the string
+    reverse(infix.begin(),infix.end());
+    //flip opening bracket to closing bracket and vice versa
+
+    for(int i = 0;i < infix.length();i++){
+        if(infix[i] == '('){
+            infix[i] = ')';
+        }else if(infix[i] == ')'){
+            infix[i] = '(';
+        }
+    }
+
+    for(int i = 0;i < infix.length();i++){
+        if((infix[i] >= 'a' && infix[i] <= 'z') || (infix[i] >= 'A' && infix[i] <= 'Z')){
+            prefix+=infix[i];
+        }else if(infix[i] == '('){
+            s.push(infix[i]);
+        }else if(infix[i] == ')'){
+            while((s.top() != '(') && (!s.empty())){
+                char temp = s.top();
+                prefix += temp;
+                s.pop();
+            }
+
+            if(s.top() == '('){
+                s.pop();
+            }
+        }else if(is_operator(infix[i])){
+            if(s.empty()){
+                s.push(infix[i]);
+            }else{
+                if(precedence(infix[i]) > precedence(s.top())){
+                    s.push(infix[i]);
+                }else if((precedence(infix[i]) == precedence(s.top())) && (infix[i] == '^')){
+                    while((precedence(infix[i]) == precedence(s.top())) && (infix[i] == '^')){
+                            prefix+=s.top();
+                            s.pop();
+                    }
+                }else if(precedence(infix[i]) == precedence(s.top())){
+                    s.push(infix[i]);
+                }else{
+                    while(!s.empty() && (precedence(infix[i] < precedence(s.top())))){
+                        prefix+=s.top();
+                        s.pop();
+                    }
+                    s.push(infix[i]);
+                }
+            }
+        }
+    }
+
+    while(!s.empty()){
+        prefix += s.top();
+        s.pop();
+    }
+
+    reverse(prefix.begin(),prefix.end());
+    
+    return prefix;
+}
+
+int main(){
+    string infix_exp,postfix_exp;
+    cout<<"Enter infix Expression :  ";
+    cin>>infix_exp;
+
+    stack <char> stack;
+    cout<<"INFIX EXPRESSION : "<<infix_exp<<endl;
+    postfix_exp = infix_to_prefix(stack,infix_exp);
+    cout<<endl<<"PREFIX EXPRESSION : "<<postfix_exp<<endl;
+
+    return 0;
+} 
